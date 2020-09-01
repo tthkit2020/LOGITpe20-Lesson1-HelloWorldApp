ea98de202c3191ad8789c818bc9273ccfe917a1e
Julia Aasmaa <my_e_10@hotmail.com>
2020-09-01 13:19:56 +0300

    Create My Frist app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //this is a commentary
+            //Console.WriteLine("Hello, Julia!");
+            /* 1. The program ask the user's name
+             * 2. The user enters their name
+             * 3. The program greets the user by their name*/
+            Console.WriteLine("What's your name?");
+            string Name;
+            //read the input from the user
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello, " + Name);
+
+        }
+    }
+}
